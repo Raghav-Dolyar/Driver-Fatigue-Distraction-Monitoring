a5ec35934316675dafab5ccef5586c187dd7a5ba
Raghav Dolyar <raghavdolyar@gmail.com>
2025-12-24 15:20:23 +0530

    Add dotenv support for environment variables
    Load environment variables from .env file.

--- code.py
+++ code.py
@@ -11,6 +11,8 @@ from pushover import Client as PushoverClient
 from twilio.rest import Client as TwilioClient
 import face_recognition
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # ------------------ Cloud Service Credentials ------------------
 PUSHOVER_USER_KEY = os.getenv("PUSHOVER_USER_KEY")
